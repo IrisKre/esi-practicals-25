65f559d96ecc2c639426bd8d014e670e9beea34a
Iris Kreinin <irkrei@taltech.ee>
2025-02-11 14:47:05 +0200

    Add Spring Boot app

---
+++ demo/src/test/java/io/esi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.esi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
